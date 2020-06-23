0ea79d35a96542b9ad45431ec374c05d66c35197
Ameer255 <64036480+Ameer255@users.noreply.github.com>
2020-06-23 11:32:16 +0500

    Add files via upload

---
+++ portfolio.js
@@ -0,0 +1,25 @@
+
+
+function navcolapse()
+{
+    var a=document.getElementById("nav-sm");
+    a.style.display="block";
+    
+    var b=document.getElementById("hamberger");
+    b.style.display="none";
+    
+     var c=document.getElementById("close");
+    c.style.display="block";
+}
+
+function clos()
+{
+      var a=document.getElementById("nav-sm");
+    a.style.display="none";
+    
+     var c=document.getElementById("close");
+    c.style.display="none";
+    
+     var b=document.getElementById("hamberger");
+    b.style.display="block";
+}
